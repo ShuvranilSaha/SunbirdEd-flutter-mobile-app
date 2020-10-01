df54b5cddcaed857d7b8f9552a5eb4337f65c037
devarfath <dev.arfath@gmail.com>
2020-10-01 15:05:23 +0530

    ADDED: qr_scan_screen styles

--- lib/pages/qr_scanner_screen/qr_scanner_screen.dart
+++ lib/pages/qr_scanner_screen/qr_scanner_screen.dart
@@ -1,5 +1,7 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:sunbird_ed_flutter_mobile_app/app_localizations.dart';
 import 'package:sunbird_ed_flutter_mobile_app/presentation/components/top_bar.dart';
 
 class QrScannerScreen extends StatefulWidget {
@@ -33,6 +35,9 @@ class _QrScannerScreenState extends State<QrScannerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        shadowColor: Colors.transparent,
+      ),
       body: Container(
         child: Stack(
           alignment: Alignment.bottomCenter,
@@ -52,33 +57,83 @@ class _QrScannerScreenState extends State<QrScannerScreen> {
             Positioned(
               top: 0,
               child: Container(
-                width: MediaQuery.of(context).size.width,
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
                 child: TopBar(
-                  MediaQuery.of(context).size.height * (1 / 3),
+                  MediaQuery
+                      .of(context)
+                      .size
+                      .height * (1 / 3),
                 ),
               ),
             ),
             Positioned(
               top: 0,
+              child: ClipRect(
+                child: Transform.translate(
+                  offset: Offset(0.0, 50.0),
+                  child: Container(
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width,
+                    height: MediaQuery
+                        .of(context)
+                        .size
+                        .height * (1 / 3),
+                    child: Container(
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                      child: FlareActor(
+                        "assets/flare/qr_scan.flr",
+                        animation: "Animation",
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              left: 0,
               child: SafeArea(
                 child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: Text("sample"),
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(AppLocalizations.of(context)
+                          .translate("SCAN_QR_CODE")),
+                      Text(AppLocalizations.of(context)
+                          .translate("SCAN_QR_INSTRUCTION")),
+                    ],
+                  ),
                 ),
               ),
             ),
             Positioned(
               bottom: 30,
               child: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: OutlineButton(child: Text('Go back', style: TextStyle(color: Colors.black),), onPressed: () {
+                child: FlatButton(
+                  color: Color(0xCC000000),
+                  child: Row(
+                    children: [
+                      Text(
+                        AppLocalizations.of(context).translate("NO_QR_CODE"),
+                      ),
+                      SizedBox(width: 8.0),
+                      Icon(Icons.info_outline)
+                    ],
+                  ),
+                  onPressed: () {
                     Navigator.of(context).pop();
-                  },),
+                  },
                 ),
               ),
             ),
-
           ],
         ),
       ),
